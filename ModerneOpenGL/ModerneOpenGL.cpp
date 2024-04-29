7f1efb8ebd1737be1295fbf01d97f3f2e36fbb2e
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2024-04-29 13:20:24 +0200

    (Add) Mouse input detection

--- ModerneOpenGL/ModerneOpenGL.cpp
+++ ModerneOpenGL/ModerneOpenGL.cpp
@@ -1,6 +1,5 @@
 // ModerneOpenGL.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 
-
 #include "Assertion.h"
 //#include <cstdint>
 #include "Log.h"
@@ -13,74 +12,74 @@
 
 int Init()
 {
-    GLFWwindow* window;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return - 1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Modern OpenGL", NULL, NULL);
-    if (!window)
-    {
-        std::cout << "Cannot create GLFW Window... \nAborting!" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Modern OpenGL", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Cannot create GLFW Window... \nAborting!" << std::endl;
+		glfwTerminate();
+		return -EXIT_FAILURE;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-    /* Init glad after context is defined */
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+	/* Init glad after context is defined */
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD... \nAborting!" << std::endl;
+		return -EXIT_FAILURE;
+	}
 
-    glfwSetKeyCallback(window, InputHandler::KeyCallback);
+	/* Handle Keyboard & Mouse Inputs */
+	glfwSetKeyCallback(window, InputHandler::KeyboardCallback);
+	glfwSetCursorPosCallback(window, InputHandler::MouseCallback);
 
-    Application app;
-    app.Initialise();
+	Application app;
+	app.Initialise();
 
-    // Set background color to blue
-    glClearColor(0.15f, 0.15f, 1.f, 1.f);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        app.Update();
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		// Set background color to blue
+		glClearColor(0.15f, 0.15f, 1.f, 1.f);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+		/* Render here */
+		app.Update();
 
-        if (InputHandler::IsKeyPressed(GLFW_KEY_SPACE))
-            std::cout << "Key Pressed" << std::endl;
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
 
-        if (InputHandler::IsKeyDown(GLFW_KEY_SPACE))
-            std::cout << "Key Down" << std::endl;
+		if (InputHandler::IsKeyPressed(GLFW_KEY_ESCAPE))
+			return EXIT_SUCCESS;
 
-        if (InputHandler::IsKeyReleased(GLFW_KEY_SPACE))
-            std::cout << "Key Released" << std::endl;
+		std::cout << "Mouse pos X : " << InputHandler::GetMousePos().x << std::endl;
+		std::cout << "Mouse pos Y : " << InputHandler::GetMousePos().y << std::endl;
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
 
-    app.Terminate();
-    glfwTerminate();
-    return 0;
+	app.Terminate();
+	glfwTerminate();
+	return 0;
 }
 
 int main()
 {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    Init();
+	Init();
 
-    Log::Get().Destroy();
+	Log::Get().Destroy();
 
-    return 0;
+	return EXIT_SUCCESS;
 }
--- ModerneOpenGL/Source/src/Core/InputHandler.cpp
+++ ModerneOpenGL/Source/src/Core/InputHandler.cpp
@@ -1,10 +1,27 @@
 #include "InputHandler.h"
+constexpr int INPUTHANDLER_NONE = -1;
 
 std::vector<int> InputHandler::m_CurrentKeyStatus(400, 0);
+std::vector<int> InputHandler::m_OldKeyStatus(400, 0);
+Vector2D InputHandler::mousePos(0.f, 0.f);
 
-void InputHandler::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+InputHandler::~InputHandler()
+{
+	m_CurrentKeyStatus.clear();
+	m_OldKeyStatus.clear();
+
+	delete &m_CurrentKeyStatus;
+	delete &m_OldKeyStatus;
+}
+
+void InputHandler::KeyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	m_CurrentKeyStatus[key] = action;
+}
+
+void InputHandler::MouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	SetMousePos(xpos, ypos);
 }
 
 bool InputHandler::IsKeyPressed(int key)
@@ -20,4 +37,9 @@ bool InputHandler::IsKeyDown(int key)
 bool InputHandler::IsKeyReleased(int key)
 {
 	return m_CurrentKeyStatus[key] == GLFW_RELEASE;
+}
+
+bool InputHandler::IsKeyUnused(int key)
+{
+	return m_CurrentKeyStatus[key] == INPUTHANDLER_NONE;
 }
