8e33339bc725eab515a1a463aaf36acca15d3676
Antoine CHARRUAU <a.charruau@student.isartdigital.com>
2024-04-30 10:06:33 +0200

    add IResource Texture

--- ModerneOpenGL/Source/include/Resources/Texture.h
+++ ModerneOpenGL/Source/include/Resources/Texture.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "ResourcesManager.h"
+
+uint32_t mipLevels;
+VkImage textureImage;
+
+class  Textures : public IResource
+{
+public:
+	Textures(std::filesystem::path const& filename)
+
+};
+
+
